b948b47e397cb5f73342ff7b352726b5319848f1
harsh12chauhan <harshchauhan0225@gmail.com>
2024-11-18 22:33:14 +0530

    variable class for making global variables

---
+++ Variables.java
@@ -0,0 +1,10 @@
+package com.projectexp0060;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.projectexp0060.user.User;
+
+public class Variables {
+	public static List<User> users = new ArrayList<>();
+}
