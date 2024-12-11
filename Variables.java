b13a4411b9a3f1018cb86012e1a9038d72070fef
harsh12chauhan <harshchauhan0225@gmail.com>
2024-12-11 16:14:19 +0530

    created array list of users and expenses

--- Variables.java
+++ Variables.java
@@ -3,8 +3,10 @@ package com.projectexp0060;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.projectexp0060.expense.Expense;
 import com.projectexp0060.user.User;
 
 public class Variables {
 	public static List<User> users = new ArrayList<>();
+	public static List<Expense> expenses = new ArrayList<>();
 }
